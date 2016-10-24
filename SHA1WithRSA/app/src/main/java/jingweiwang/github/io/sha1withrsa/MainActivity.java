c3eacf706e6c0284de42c67a314d00fc00ea7874
JingweiWang <13622032005@qq.com>
2016-10-25 00:42:47 +0800

    Upgraded SHA1WithRSA.

--- SHA1WithRSA/app/src/main/java/jingweiwang/github/io/sha1withrsa/MainActivity.java
+++ SHA1WithRSA/app/src/main/java/jingweiwang/github/io/sha1withrsa/MainActivity.java
@@ -9,9 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText et_input;
-    Button btn_sign;
-    TextView tv_signed;
+    private static final String ASSETS_PATH_FOR_PRIAVTE_KEY = "key/private_key_pkcs8.pem";
+    private static final String ASSETS_PATH_FOR_PUBLIC_KRY = "key/public_key.pem";
+    private EditText et_input;
+    private Button btn_sign, btn_verify;
+    private TextView tv_signed, tv_verified;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et_input = (EditText) findViewById(R.id.et_input);
         btn_sign = (Button) findViewById(R.id.btn_sign);
+        btn_verify = (Button) findViewById(R.id.btn_verify);
         tv_signed = (TextView) findViewById(R.id.tv_signed);
+        tv_verified = (TextView) findViewById(R.id.tv_verified);
 
         btn_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String str = et_input.getText().toString();
                 Log.e("str", str);
-                String strSigned = SHA1WithRSA.getBase64(getApplicationContext(), str);
+                String strSigned = SHA1WithRSA.signWithBase64(getApplicationContext(), ASSETS_PATH_FOR_PRIAVTE_KEY, str);
                 tv_signed.setText(strSigned);
+            }
+        });
+
+        btn_verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = et_input.getText().toString();
+                Log.e("str", str);
+                String signed = tv_signed.getText().toString();
+                Log.e("signed", signed);
+                boolean result = SHA1WithRSA.verify(getApplicationContext(), ASSETS_PATH_FOR_PUBLIC_KRY, str, signed);
+                if (result) {
+                    tv_verified.setText("验证成功");
+                } else {
+                    tv_verified.setText("验证失败");
+                }
             }
         });
     }
--- SHA1WithRSA/app/src/main/java/jingweiwang/github/io/sha1withrsa/SHA1WithRSA.java
+++ SHA1WithRSA/app/src/main/java/jingweiwang/github/io/sha1withrsa/SHA1WithRSA.java
@@ -5,70 +5,103 @@ import android.util.Base64;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.Signature;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Created by wangjingwei on 2016/10/23.
  */
 
 public class SHA1WithRSA {
-    private static Signature sign = null;
-    private static PrivateKey privateKey;
-
     private SHA1WithRSA() {
+    }
+
+    private static String readAssetsKey(Context context, String assetsPath) throws IOException {
+        InputStreamReader inputReader = new InputStreamReader(context.getResources().getAssets().open(assetsPath));
+        BufferedReader bufReader = new BufferedReader(inputReader);
+        String line;
+        String result = "";
+        while ((line = bufReader.readLine()) != null) {
+            result += line;
+        }
+        return result;
+
     }
 
     private static PrivateKey getPrivateKey(byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-        return privateKey;
+        return keyFactory.generatePrivate(keySpec);
     }
 
+    private static PublicKey getPublicKey(byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
+    }
+
+
     /**
-     * @param context  the activity context
-     * @param posttemp the String for sign with SHA1WithRSA
-     * @return signed string showed by base64
+     * 获取字符串的 Base64 格式的 SHA1WithRSA 签名
+     *
+     * @param context                 activity context
+     * @param assetsPathForPrivateKey such as "key/private_key_pkcs8.pem", pkcs#8 format
+     * @param oriStr                  String for sign with SHA1WithRSA
+     * @return signed string by base64
      */
-    public static String getBase64(Context context, String posttemp) {
-        /**
-         * take private.pem to pkcs#8 by openssl to the temp
-         */
-        String temp = "";
+    public static String signWithBase64(Context context, String assetsPathForPrivateKey, String oriStr) {
         try {
-            InputStreamReader inputReader = new InputStreamReader(context.getResources().getAssets().open("key/private_key_pkcs8.pem"));
-            BufferedReader bufReader = new BufferedReader(inputReader);
-            String line = "";
-            String result = "";
-            while ((line = bufReader.readLine()) != null) {
-                result += line;
-            }
-            temp = result;
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("SHA1WithRSA_to_BASE64", e.getMessage());
-        }
-        String pkcs8 = temp.replace("-----BEGIN PRIVATE KEY-----", "")
-                .replace("-----END PRIVATE KEY-----", "");
-        byte[] bytetemp = null;
-        try {
-            bytetemp = Base64.decode(pkcs8, Base64.DEFAULT);
-            privateKey = getPrivateKey(bytetemp);
-            sign = Signature.getInstance("SHA1WithRSA");
+            String pkcs8 = readAssetsKey(context, assetsPathForPrivateKey)
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "");
+            byte[] keyBytes = Base64.decode(pkcs8, Base64.DEFAULT);
+            PrivateKey privateKey = getPrivateKey(keyBytes);
+            Signature sign = Signature.getInstance("SHA1WithRSA");
             sign.initSign(privateKey);
-            sign.update(posttemp.getBytes());
+            sign.update(oriStr.getBytes());
             byte[] data = sign.sign();
             return Base64.encodeToString(data, Base64.DEFAULT).replace("\n", "").replace("\t", "").replace(" ", "").trim();
-        } catch (Exception e) {
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeySpecException | InvalidKeyException | IOException e) {
             e.printStackTrace();
             Log.e("SHA1WithRSA_to_BASE64", e.getMessage());
         }
         return "error";
+    }
+
+    /**
+     * 验证签名是否与原字符串匹配
+     *
+     * @param context                activity context
+     * @param assetsPathForPublicKey such as "key/public_key.pem"
+     * @param oriStr                 String for verify with SHA1WithRSA
+     * @param signedStr              signed string by base64
+     * @return true if verify pass
+     */
+    public static boolean verify(Context context, String assetsPathForPublicKey, String oriStr, String signedStr) {
+        try {
+            String publicKeyStr = readAssetsKey(context, assetsPathForPublicKey)
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "");
+            byte[] keyBytes = Base64.decode(publicKeyStr, Base64.DEFAULT);
+            PublicKey publicKey = getPublicKey(keyBytes);
+            Signature sign = Signature.getInstance("SHA1WithRSA");
+            sign.initVerify(publicKey);
+            sign.update(oriStr.getBytes());
+            return sign.verify(Base64.decode(signedStr, Base64.DEFAULT));
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeySpecException | InvalidKeyException | IOException e) {
+            e.printStackTrace();
+            Log.e("SHA1WithRSA_to_BASE64", e.getMessage());
+        }
+        return false;
     }
 }
