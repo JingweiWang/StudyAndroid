a8b18f6e6e410d9a7f1694dbabb3c0aff7f22f59
JingweiWang <13622032005@qq.com>
2016-11-03 17:53:51 +0800

    Added ServiceLifeCycle.

---
+++ ServiceLifeCycle/app/src/main/java/io/github/jingweiwang/servicelifecycle/ServiceForBind.java
@@ -0,0 +1,51 @@
+package io.github.jingweiwang.servicelifecycle;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ServiceForBind extends Service {
+    private static final String TAG = ServiceForBind.class.getSimpleName();
+    InnerBinder binder = new InnerBinder();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG, "onCreate in " + Thread.currentThread().getName());
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand in " + Thread.currentThread().getName());
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy in " + Thread.currentThread().getName());
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e(TAG, "onBind in " + Thread.currentThread().getName());
+        return binder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.e(TAG, "onUnbind in " + Thread.currentThread().getName());
+        return super.onUnbind(intent);
+    }
+
+    public class InnerBinder extends Binder {
+        public void doSomething() {
+            Log.e(TAG, "doSomething() in " + Thread.currentThread().getName());
+        }
+    }
+
+
+}
